2f2784d73fe713f6bf9d3abf8c921832bfa9ce67
Samuele Villa <sam@esercitazioni-vm.local.vm.net>
2024-11-09 10:21:00 +0100

    PROLOG COURSE: variables & facts

---
+++ prolog/course/facts.pl
@@ -0,0 +1,23 @@
+%%%% -*- Mode: Prolog -*-
+
+% facts.pl
+
+% relationship
+% objects start with lowercase
+% knowlege base
+
+likes(john, mary). % john likes mary
+
+valuable(gold).
+
+female(jane).
+female(mary).
+
+father(jhon, mary).
+
+gives(john, book, mary).
+
+play(john, mary, fotball).
+play(jane, jim, badminton).
+
+% facts.pl ends here.
---
+++ prolog/course/variables.pl
@@ -0,0 +1,10 @@
+%%%% -*- Mode: Prolog -*-
+
+% variables.pl
+
+likes(john, juventus).
+likes(mario, inter).
+likes(jane, milan).
+likes(mary, juventus).
+
+% variables.pl ends here.
